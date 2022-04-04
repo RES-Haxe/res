be4a37f05adc10a7a1e5332069a6863c27afc0ed
Michael Miriti <m.s.miriti@gmail.com>
2022-04-05 01:30:39 +0300

    FileStorage fix

--- src/res/bios/common/FileStorage.hx
+++ src/res/bios/common/FileStorage.hx
@@ -1,46 +1,25 @@
 package res.bios.common;
 
 import haxe.Json;
-import res.storage.IStorage;
-import sys.FileSystem;
+import res.storage.Storage;
 import sys.io.File;
 
 using Reflect;
 
-class FileStorage implements IStorage {
-	var storage:Dynamic<String>;
+class FileStorage extends Storage {
 	var fileName:String;
 
 	public function new(?fileName:String = 'data.dat') {
+		super();
+
 		this.fileName = fileName;
-
-		if (FileSystem.exists(fileName)) {
-			this.storage = Json.parse(File.getContent(fileName));
-		} else {
-			this.storage = {};
-		}
 	}
 
-	public function put(key:String, data:Dynamic) {
-		storage.setField(key, data);
+	public function save() {
+		File.saveContent(fileName, Json.stringify(data));
 	}
 
-	public function get(key:String):Dynamic {
-		return storage.getProperty(key);
-	}
-
-	public function getString(key:String, ?defaultValue:String):String {
-		final stored = get(key);
-		return stored == null ? defaultValue : '$stored';
-	}
-
-	public function getInt(key:String, ?defaultValue:Int):Null<Int> {
-		final stored = get(key);
-		return stored == null ? defaultValue : Std.parseInt('$stored');
-	}
-
-	public function getFloat(key:String, ?defaultValue:Float):Null<Float> {
-		final stored = get(key);
-		return stored == null ? defaultValue : Std.parseFloat('$stored');
+	public function restore() {
+		data = Json.parse(File.getContent(fileName));
 	}
 }
